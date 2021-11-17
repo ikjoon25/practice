2490b536ce3b70949379703d5a10493178e4fa6a
ikjoon25 <93104713+ikjoon25@users.noreply.github.com>
2021-11-17 23:03:16 +0900

    var02

--- 211117/js/script.js
+++ 211117/js/script.js
@@ -3,3 +3,23 @@ $('#nav>ul>li').hover(function(){
 },function(){
     $(this).find('ul').stop().slideUp()
 });
+
+var slide = $('#slide ul li');
+var current = 0;
+
+
+
+setInterval(function(){
+    var prev = slide.eq(current);
+    current++;
+    if(current==slide.size()){
+        current=0;
+    }
+    move(prev, 0, '-100%');
+    var next = slide.eq(current);
+    move(next, '100%', 0);
+},2000);
+function move(tg, start, end){
+    tg.css('left',start).stop().animate({left:end},500)
+}
+
