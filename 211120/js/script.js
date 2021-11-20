862c7d9f834c1ac93fd6087039df11eaab33b562
ikjoon25 <93104713+ikjoon25@users.noreply.github.com>
2021-11-21 01:31:31 +0900

    var02

--- 211120/js/script.js
+++ 211120/js/script.js
@@ -4,18 +4,18 @@ $("#nav>ul>li").on({mouseover:function(){
     $(this).find('ul').stop().slideUp();
 }});
 
-var slide=$('#slide>ul>li');
+var slide=('#slide>ul>li');
 var current=0;
 
 setInterval(function(){
     var prev=slide.eq(current);
     current++;
-    if(current==slide.size()){current=0}
-    move(prev, 0, '-100%');
-    var next=slide.eq(current);
-    move(next, '100%', 0);
+    if(current==slide.size()){current=0};
+    move(prev, 0,'100%');
+    var next=slide.eq(currnet);
+    move(next,'-100%',0);
 },2000);
 
-function move(tg, start, end){
-    tg.css('left',start).stop().animate({left:end},500);
+function move(tg,start,end){
+    tg.css('left',start).stop().animate({left:end},500)
 }
